5444f26c9e65430836a67f109082adf693ca3202
ozankersit <ozankersit@gmail.com>
2025-01-05 02:40:59 +0300

    set global variables

--- src/app/fetch-recipes/action.ts
+++ src/app/fetch-recipes/action.ts
@@ -1,8 +1,9 @@
 "use server";
 
 export async function fetchRecipes(ingredients: string) {
+  const WEB_URL = process.env.NEXT_LOCAL_WEB_URL;
   try {
-    const res = await fetch("http://localhost:3000/api/recipes", {
+    const res = await fetch(`${WEB_URL}/api/recipes`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
