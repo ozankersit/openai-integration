44bff3dae2de2890080118b3e2d124cfd6f1b64f
ozankersit <ozankersit@gmail.com>
2025-04-15 18:04:34 +0300

    global error improvement

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,11 +1,18 @@
-"use client"
+"use client";
+
+import Image from "next/image";
 
 export default function GlobalError() {
   return (
     <html>
-        <body>
-        <h1>Something went wrong</h1>
-        </body>
+      <body className="h-screen flex items-center justify-center">
+        <Image
+          src={"/images/global-error.gif"}
+          width={480}
+          height={360}
+          alt="error-gif"
+        />
+      </body>
     </html>
-  )
+  );
 }
